dab37f3de40cfd20517fe430b8bb29e48cffec50
LeonidPolizharov <polizharov@mail.ru>
2022-04-26 18:22:33 +0300

    some changes for review PR-2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -11,9 +11,9 @@ MENU_LINKS = {
 # Create your views here.
 def index(request):
     return render(request, 'mainapp/index.html', context={
-        'title': 'Главная',
-        'menu': MENU_LINKS,
-         })
+      'title': 'Главная',
+      'menu': MENU_LINKS,
+       })
 
     
 def products(request):
@@ -22,15 +22,15 @@ def products(request):
     # with open('E:/Django_project/geekshop/products.json', 'r', encoding='utf-8') as f:
     #    products = json.load(f)
     return render(request, 'mainapp/products.html', context={
-        'title': 'Продукты',
-        'menu': MENU_LINKS,
-        'products': products,
-        'categories': categories,
-        })
+      'title': 'Продукты',
+      'menu': MENU_LINKS,
+      'products': products,
+      'categories': categories,
+      })
 
 
 def contact(request):
     return render(request, 'mainapp/contact.html', context={
-        'title': 'Контакты',
-        'menu': MENU_LINKS,
-        })
+      'title': 'Контакты',
+      'menu': MENU_LINKS,
+      })

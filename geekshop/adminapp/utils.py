fd10649a26ee4bd6555a8c98b88cf5448a076361
LeonidPolizharov <polizharov@mail.ru>
2022-05-18 18:30:28 +0300

    lesson-7

---
+++ geekshop/adminapp/utils.py
@@ -0,0 +1,4 @@
+from django.contrib.auth.decorators import user_passes_test
+
+
+check_is_superuser = user_passes_test(lambda u: u.is_superuser)
--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=32, unique=True)
     description = models.CharField(max_length=140)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.id}: {self.name}'
@@ -15,6 +16,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product_image/')
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.id}: {self.name}'

3c1490b61fe80ee97054cfa2da7325774e279592
LeonidPolizharov <polizharov@mail.ru>
2022-06-25 15:57:24 +0300

    .env fixed 3

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -91,15 +91,8 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
+USE_POSTGRES = bool(int(os.environ.get('USE_POSTGRES', True)))
+if USE_POSTGRES:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -108,8 +101,15 @@ else:
         'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'geekshop'),
         'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
         'PORT': os.environ.get('DATABASE_PORT', ''),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
